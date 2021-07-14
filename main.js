a813b444957d563923373b33228f003ad7838b75
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-07-14 18:50:29 +0900

    21.07.14(수) 유튜브 클론코딩
    드림코딩 엘리님 강의를 보며 5시간동안 빡세게 집중해서 프로젝트 하나를 끝냈다.
    뭔가 보여줄 수 있는 플랫폼이 있어서 다행이다.

---
+++ main.js
@@ -0,0 +1,10 @@
+
+/* 아직 JS는 제대로 배우지 못했습니다 */
+
+const moreBtn = document.querySelector('.info .metadata .moreBtn');
+const title = document.querySelector('.info .metadata .title');
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+});
